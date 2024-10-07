1530780cc8a97653f3e290e5341efec27bed4089
Friederish <friederishmartinezramirez@gmail.com>
2024-10-07 14:36:35 -0600

    Mostrar informacion, metodo de extension publico

--- Extension.cs
+++ Extension.cs
@@ -6,8 +6,21 @@ using System.Threading.Tasks;
 
 namespace Sistema
 {
-    static class EstudiantesExtension 
+    static class EstudiantesExtension
     {
+        public static void MostrarInformacion(this Sistema.Universidad.Estudiante estudiante)
+        {
+            double porcentajeAsistencia = Sistema.Universidad.Estudiante.Asistencia.CalcularPorcentaje
+                           (estudiante.SesionesTotales, estudiante.SesionesAsistidas);
 
+            bool cumpleMinimo = Sistema.Universidad.Estudiante.Asistencia.CumpleMinimoAsistencia
+                (estudiante.SesionesTotales, estudiante.SesionesAsistidas);
+
+            int asistenciamax = Sistema.Universidad.Estudiante.Asistencia.CumpMax
+                (estudiante.SesionesTotales, estudiante.SesionesAsistidas);
+
+            int sesionesAsistidas = Sistema.Universidad.Estudiante.Asistencia.SA
+                 (estudiante.SesionesTotales, estudiante.SesionesAsistidas);
+        }
     }
 }
