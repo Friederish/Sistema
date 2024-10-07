ecb7b2be216a0d55fa3112ff9e1d79aafe9dc753
Friederish <friederishmartinezramirez@gmail.com>
2024-10-07 14:29:08 -0600

    Clase Anidada estudiantes

--- Universidaad.cs
+++ Universidaad.cs
@@ -8,6 +8,32 @@ namespace Sistema
 {
     public class Universidad
     {
-       
+        public class Estudiante //Clase anidada denominada "Estudiante"
+        {
+            public string Nombre { get; set; }
+            public int SesionesTotales { get; set; }        //Propiedades
+            public int SesionesAsistidas { get; set; }
+
+            internal class Asistencia //Clase interna "Asistencia" de la clase "Estudiante"
+            {
+                public static double CalcularPorcentaje(int sesionesTotales, int sesionesAsistidas)
+                {
+                    return (double)sesionesAsistidas / sesionesTotales * 100;
+                }
+
+                public static bool CumpleMinimoAsistencia(int sesionesTotales, int sesionesAsistidas)
+                {
+                    return CalcularPorcentaje(sesionesTotales, sesionesAsistidas) >= 75;
+                }
+                public static int CumpMax(int sesionesTotales, int sesionesAsistidas)
+                {
+                    return (sesionesTotales);
+                }
+                public static int SA(int sesionesTotales, int sesionesAsistidas)
+                {
+                    return (sesionesAsistidas);
+                }
+            }
+        }
     }
 }
